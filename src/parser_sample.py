9be5982c88923f636720f912ef86a9ec019657bd
Martin <martin.toutant1@gmail.com>
2021-05-10 18:17:08 +0200

    bugfix

--- src/parser_sample.py
+++ src/parser_sample.py
@@ -7,11 +7,11 @@ from pypeg2 import *
 
 # Il reste a implémenter extension-method --> à voir
 # Enum ne marche qu'avec des keywords
-class Mehod(Keyword):
+class Method(Keyword):
     grammar = Enum(K("GET"), K("HEAD"), K("POST"), K("PUT"), K("DELETE"), K("TRACE"), K("CONNECT"), K("OPTIONS"))
 
 class Digit:
-    grammar = re.comile(r"[0-9]")
+    grammar = re.compile(r"[0-9]")
 
 class SubDelimiter(Keyword):
     grammar = Enum(K("!"), K("$"), K("&"), K("'"), K("("), K(")"), K("*"), K("+"), K(","), K(";"), K("="))
@@ -19,8 +19,8 @@ class SubDelimiter(Keyword):
 # Alpha | Digit 
 #   <=> [a-zA-Z0-9_] 
 #   <=> \w
-class Unreserverd:
-    grammar = [re.compile(r"\w"), "-", ".", "_", "~"]
+class Unreserved:
+    grammar = re.compile(r"\w"), "-", ".", "_", "~"
 
 # Pour host, on ignore pct-encoded
 class RegularName:
@@ -67,10 +67,19 @@ class HierPart:
 class RequestURI:
     grammar = re.compile(r"^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?")
 
-class HttpVer
+class HttpVer:
+    grammar = "HTTP", "/", some(Digit), ".", some(Digit)
 
 class ReqLine:
     grammar = attr("method", Method), whitespace, attr("request_uri", RequestURI), whitespace, attr("http_version", HttpVer), endl
 
+#DEBUG
+class Header:
+    grammar = re.compile(r"*")
+
+class MessageBody:
+    grammar = re.compile(r"*")
+
 class Request:
     grammar = attr("req_line", ReqLine), maybe_some(attr("header", Header), endl), endl, optional(attr("mess_body", MessageBody))
+
